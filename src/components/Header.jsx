58e0711f81c887eb9a52706939e205c7d4e9be03
okorojames <okorojameschizaram@gmail.com>
2023-02-10 22:34:58 +0100

    working on the nav header

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div className="navLogo">News<span className="navLogoColor">Media</span></div>
+        {/*  */}
+        <form className="searchForm">
+          <input type="search" placeholder="search" className="searchInput" />
+        </form>
+        {/*  */}
+        <div className="navIcons">
+            <i className="ri-twitter-fill navIcon"></i>
+            <i className="ri-facebook-fill navIcon"></i>
+            <i className="ri-instagram-line navIcon"></i>
+        </div>
+        {/*  */}
+        <div className="hamburger">
+          <div className="burger burgerOne"></div>
+          <div className="burger burgerTwo"></div>
+          <div className="burger burgerThree"></div>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
