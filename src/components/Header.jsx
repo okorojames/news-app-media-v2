c6f8260f34ff35dde6faeeccc951c3ac84036a44
okorojames <okorojameschizaram@gmail.com>
2023-02-11 03:37:34 +0100

    working on the nav media query

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
+import { useState, useRef } from "react"
+import SideNav from './SideNav'
 const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false)
+  const navWrapper = useRef()
+  function openNav() {
+    setIsNavOpen(!isNavOpen)
+    navWrapper.classList.toggle('is-nav-open')
+  }
   return (
     <header>
       <nav>
-        <div className="navLogo">News<span className="navLogoColor">Media</span></div>
+        <div ref={navWrapper} className="navLogo">News<span className="navLogoColor">Media</span></div>
         {/*  */}
         <div className="navContentDetails">
         {/*  */}
@@ -10,20 +18,28 @@ const Header = () => {
           <i className="ri-search-line"></i>
           <input type="search" placeholder="search" className="searchInput" />
         </form>
-        {/*  */}
+          {/*  */}
+          <div className="navIconsHamburger">
+          {/*  */}
         <div className="navIcons">
             <i className="ri-twitter-fill navIcon"></i>
             <i className="ri-facebook-fill navIcon"></i>
             <i className="ri-instagram-line navIcon"></i>
         </div>
         {/*  */}
-        <div className="hamburger">
+        <div className="hamburger" onClick={openNav}>
           <div className="burger burgerOne"></div>
           <div className="burger burgerTwo"></div>
           <div className="burger burgerThree"></div>
-          </div>
+            </div>
+            {/*  */}
+            </div>
           {/*  */}
-          </div>
+        </div>
+        {isNavOpen ?
+          <SideNav />
+          :
+          false}
       </nav>
     </header>
   )
---
+++ src/components/SideNav.jsx
@@ -0,0 +1,14 @@
+const SideNav = () => {
+  return (
+    <div className="sideNav">
+      <a href="#" className="sideNavLink">Home</a>
+      <a href="#" className="sideNavLink">Categories</a>
+      <a href="#" className="sideNavLink">Travel</a>
+      <a href="#" className="sideNavLink">Food</a>
+      <a href="#" className="sideNavLink">Technology</a>
+      <a href="#" className="sideNavLink">Business</a>
+    </div>
+  )
+}
+
+export default SideNav
